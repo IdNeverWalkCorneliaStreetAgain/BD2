49f04bcb47d7335d06347147d198cc6e21fc632e
Raffaella Calò <95417543+IdNeverWalkCorneliaStreetAgain@users.noreply.github.com>
2023-07-04 16:42:55 +0200

    Update QueryDb.py
    Aggiunte queries complesse

--- QueryDb.py
+++ QueryDb.py
@@ -73,3 +73,114 @@ def deleteUser(user_id):
 def checkUser(idUser):
     found = db.user.find_one({"user_id": idUser})
     return found
+
+#Restituisce tutti i libri pubblicati nell'anno preso in input
+def query1(anno): 
+    queryResult = db.book.find({ "year_of_publication": anno})
+    return queryResult, 1
+
+#Recupera i dettagli di un libro, incluso il nome dell'autore e il rating medio
+def query2(isbn):
+    result = list(db.book.aggregate([
+        {
+            "$match": {"isbn" : isbn}
+            },
+        {
+            "$lookup" : {
+                "from" : "rating",
+                "localField" : "isbn",
+                "foreignField" : "isbn",
+                "as" : "ratings"
+             }
+            },
+        {
+            "$unwind" : "$ratings"
+            },
+        {
+            "$group" : {
+                "_id" : "$isbn",
+                "book_title" : {"$first" : "$book_title"},
+                "book_author" : {"$first" : "$book_author"},
+                "averange_rating" : {"$avg" : "$ratings.rating"}
+                }
+            }
+        ]))
+    return result, 2
+
+#Restituisce tutti i libri di una data casa editrice che hanno ottenuto
+#un punteggio superiore a quanto dato in input
+def query3(editore, punteggio):
+    result = list(db.book.aggregate([
+        {
+            "$match" : {"publisher" : editore}
+            },
+        {
+            "$lookup" : {
+                "from" : "rating",
+                "localField" : "isbn",
+                "foreignField" : "isbn",
+                "as" : "ratings"
+               }
+            },
+        {
+            "$unwind" : "$ratings"
+        },
+        {
+            "$match" : {"ratings.rating" : {"$gt" : punteggio}}
+            },
+        {
+            "$project" : {
+                "_id" : 0,
+                "isbn" : 1,
+                "book_title" : 1,
+                "book_author" : 1,
+                "year_of_publication" : 1,
+                "publisher" : 1,
+                "Category" : 1
+                }
+            }
+        ]))
+    return result, 3
+
+#Restituisce tutti gli utenti che abitano in una determinata città e hanno
+#un'età inferiore a 30 anni
+def query4(città):
+    queryResult = db.user.find({
+        "city" : città,
+        "age" : {"$lt" : 30}
+        })
+    return queryResult, 4
+
+#Restituisce tutti gli utenti che hanno assegnato ad un deteminato libro un
+#punteggio superiore a quello ricevuto in input
+def query5(isbn, punteggio):
+    queryResult = list(db.rating.aggregate([
+        {
+            "$match" : {
+                "isbn" : isbn,
+                "rating" : {"$gt" : punteggio}
+                }
+            },
+        {
+            "$lookup" : {
+                "from" : "user",
+                "localField" : "user_id",
+                "foreignField" : "user_id",
+                "as" : "userDet"
+    }
+            },
+        {
+            "$unwind" : "$userDet"
+    },
+        {
+            "$project": {
+                "_id": 0,
+                "user_id": "$userDet.user_id",
+                "age": "$userDet.age",
+                "city": "$userDet.city",
+                "country": "$userDet.country",
+                "state": "$userDet.state"
+    }
+  }
+        ]))
+    return queryResult, 5
